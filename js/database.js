5e88d094c8669397906f83fb82f0c079dfe55433
thamudhifernando <fernandothamudhi@gmail.com>
2019-05-17 12:02:27 +0530

    initial commit

---
+++ js/database.js
@@ -0,0 +1,42 @@
+var courses = [
+    {
+        course_id:"C001",
+        course_name:"DEP",
+        course_description:"Derect Entry Point",
+        course_duration:"6-months"
+    },
+    {
+        course_id:"C002",
+        course_name:"GDSE",
+        course_description:"Graduate Diploma in Software Engineering",
+        course_duration:"2-years"
+    }
+
+];
+var batchs = [
+    {
+        course_id:"C001",
+        course_name:"GDSE",
+        batch_id:"B001",
+        batch_name:"GDSE-1"
+    }
+
+];
+var students=[
+    {
+        student_id: "S001",
+        student_name: "Vishva",
+        student_contact: "0712568421"
+    }
+];
+
+var register=[
+    {
+        student_id: "S001",
+        student_name: "Vishva",
+        course_id:"C001",
+        course_name:"GDSE",
+        batch_id:"B001",
+        batch_name:"GDSE-1"
+    }
+];
